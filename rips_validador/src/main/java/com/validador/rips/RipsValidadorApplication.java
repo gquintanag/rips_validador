93ce8758ec9d10162b2b752d11d9cff0942766cd
GABRIEL-QUINTANA <GABRIEL-QUINTANA@GABRIEL>
2021-08-08 23:47:31 -0500

    commit inical con eclipse

---
+++ rips_validador/src/main/java/com/validador/rips/RipsValidadorApplication.java
@@ -0,0 +1,13 @@
+package com.validador.rips;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RipsValidadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RipsValidadorApplication.class, args);
+	}
+
+}
---
+++ rips_validador/src/test/java/com/validador/rips/RipsValidadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.validador.rips;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RipsValidadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
